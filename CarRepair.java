8c071d2a2ee9efe818aa4b4220fb7e84aed5b6da
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-05-25 21:15:27 -0400

    ev

---
+++ CarRepair.java
@@ -0,0 +1,5 @@
+public class CarRepair extends Illness{
+    public CarRepair(String name, String description, String treatment, String[] symptoms, String[] causes) {
+        super(name, description, treatment, symptoms, causes);
+    }
+}
